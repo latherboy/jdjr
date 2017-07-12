ea9ff14d00a74455acddea2129d3165d92ba0ab4
latherboy <10301428@qq.com>
2017-07-12 17:08:11 +0800

    还差一个页面

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-var mySwiper = new Swiper('.swiper-container', {
-    autoplay: 3000,//可选选项，自动滑动
-    pagination : '.swiper-pagination',
-    loop:true,
-	autoplayDisableOnInteraction : false,
-})
+$(function() {
+	var mySwiper = new Swiper('.swiper-container', {
+		autoplay: 3000,//可选选项，自动滑动
+		pagination: '.swiper-pagination',
+		loop: true,
+		speed:2000,
+		autoplayDisableOnInteraction: false,
+		observer:true,
+		observeParents:true
+	});
+});
--- js/life.js
+++ js/life.js
@@ -1,56 +1,63 @@
 $(function() {
-	var mySwiper1 = new Swiper('.treasure_banner', {
+	var mySwiper1 = new Swiper('.life_banner', {
 		autoplay: 3000,//可选选项，自动滑动
 		pagination: '.swiper-pagination',
 		loop: true,
-		speed:2000,
+		speed:1500,
 		autoplayDisableOnInteraction: false,
 		observer:true,
 		observeParents:true
 	});
-	var mySwiper2 = new Swiper('.jjjx_banner', {
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		spaceBetween: 30,
-		observer:true,
-		observeParents:true
-	});
-	var mySwiper3 = new Swiper('.baoxian_banner', {
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		spaceBetween: 30,
-		observer:true,
-		observeParents:true
-	});
-	var mySwiper4 = new Swiper('.tslc_banner', {
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		spaceBetween: 30,
-		observer:true,
-		observeParents:true
-	});
-	var mySwiper5 = new Swiper('.tjyd_banner1', {
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		spaceBetween: 30,
-		observer:true,
-		observeParents:true
-	});
-	var mySwiper6 = new Swiper('.tjyd_banner2', {
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		spaceBetween: 30,
-		observer:true,
-		observeParents:true
-	});
-	var mySwiper7 = new Swiper('.gdxx_b', {
-		autoplay: 3000,
+	var mySwiper2 = new Swiper('.laba_banner', {
+		autoplay:1500,
 		direction: 'vertical',
 		slidesPerView: 'auto',
 		loop: true,
 		speed: 1000,
 		paginationClickable: true,
 		autoplayDisableOnInteraction: false,
+		observer:true,
+		observeParents:true
+	});
+	var mySwiper3 = new Swiper('.zchw_banner', {
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		spaceBetween: 0,
+		observer:true,
+		observeParents:true
+	});
+	var mySwiper4 = new Swiper('.classify', {
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		spaceBetween: 30,
+		observer:true,
+		observeParents:true
+	});
+	var mySwiper5 = new Swiper('.baoxian_banner', {
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		spaceBetween: 30,
+		observer:true,
+		observeParents:true
+	});
+	var mySwiper6 = new Swiper('.jddz_banner', {
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		spaceBetween: 30,
+		observer:true,
+		observeParents:true
+	});
+	var mySwiper7 = new Swiper('.fanli_banner', {
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		spaceBetween: 30,
+		observer:true,
+		observeParents:true
+	});
+	var mySwiper8 = new Swiper('.hwzj_banner', {
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		spaceBetween: 30,
 		observer:true,
 		observeParents:true
 	});
